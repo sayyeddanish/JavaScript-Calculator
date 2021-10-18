834304c4df63e424ce669611b18cc8232f5acca2
Danish <danish.sayyed@novoinvent.com>
2021-10-18 11:33:15 +0530

    calculation functionality added

--- calculator.js
+++ calculator.js
@@ -1,3 +1,23 @@
-function enterNumber(numCheck){
-    var x=numCheck;
+function enterNumber(numCheck,inputType){
+    var total_val=document.getElementById('input_val').value;
+    var prevVal=total_val.substr(total_val.length-1);
+    if(inputType=='dec')
+    {
+        if(total_val.indexOf('.')!='-1')
+        return;
+    }
+    if(prevVal=='*' || prevVal=='/' || prevVal=='-' || prevVal=='+'  )
+    {
+        if(numCheck=='*' || numCheck=='/' || numCheck=='+' || numCheck=='-')
+        return;
+    }
+    document.getElementById('input_val').value +=numCheck;
+}
+
+function equalTo(){
+    var total_val=document.getElementById('input_val').value;
+    document.getElementById('input_val').value=eval(total_val);
+}
+function clearNumber(){
+    document.getElementById('input_val').value ='';
 }
